07b304aa709a1a2b4f0e5f892fa3cd55dd7ffd33
mitje-glavanakov <72776036+mitje-glavanakov@users.noreply.github.com>
2022-01-28 02:17:18 +0300

    pew

--- banki/scripts/main.js
+++ banki/scripts/main.js
@@ -1,6 +1,15 @@
 var path = 'images/',
     imgs = '.png',
-    i = String(Math.floor(Math.random()*8));
+    i = String(Math.floor(Math.random()*7));
+    j = String(Math.floor(Math.random()*7));
+    k = String(Math.floor(Math.random()*7));
+    l = String(Math.floor(Math.random()*7));
     console.log();
-$('.gallery').append("<img src='"+path+i+imgs+"' alt='my partner got in the shot accidentally'>");
+$('.gallery1').append("<img src='"+path+i+imgs+"' alt='my partner got in the shot accidentally'>");
+console.log();
+$('.gallery2').append("<img src='"+path+j+imgs+"' alt='my partner got in the shot accidentally'>");
+console.log();
+$('.gallery3').append("<img src='"+path+k+imgs+"' alt='my partner got in the shot accidentally'>");
+console.log();
+$('.gallery4').append("<img src='"+path+l+imgs+"' alt='my partner got in the shot accidentally'>");
 console.log();
