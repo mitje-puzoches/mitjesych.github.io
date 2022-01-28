a82d9fec186c68b2a3bce296336f9f632cb77495
mitje-glavanakov <72776036+mitje-glavanakov@users.noreply.github.com>
2022-01-28 03:45:56 +0300

    pewpew

--- shlagbaum/scripts/main.js
+++ shlagbaum/scripts/main.js
@@ -1,6 +1,15 @@
 var path = 'images/',
     imgs = '.jpg',
     i = String(Math.floor(Math.random()*7));
+    j = String(Math.floor(Math.random()*7));
+    k = String(Math.floor(Math.random()*7));
+    l = String(Math.floor(Math.random()*7));
     console.log();
-$('.gallery').append("<img src='"+path+i+imgs+"'>");
+$('.gallery1').append("<img src='"+path+i+imgs+"' >");
+console.log();
+$('.gallery2').append("<img src='"+path+j+imgs+"' >");
+console.log();
+$('.gallery3').append("<img src='"+path+k+imgs+"' >");
+console.log();
+$('.gallery4').append("<img src='"+path+l+imgs+"' >");
 console.log();
